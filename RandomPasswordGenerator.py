3e99c5f053cccba97c00e247f53af2f8b0a90468
Shobhit Nautiyal <84954282+Shobhit1812@users.noreply.github.com>
2021-06-20 10:40:41 +0530

    Update RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -3,7 +3,7 @@ import random
 lower="qwertyuioplkjhgfdsazxcvbnm"
 upper="QWERTYUIOPLKJHGFDSAZXCVBNM"
 numbers="0123456789"
-symbol="!@#$%^&*()_-+={[}]\|;:'""<,.>/?"
+symbol="!@#$%^&*()_-+={[}]<>/"
 all=lower+upper+numbers+symbol
 length=16
 password="".join(random.sample(all,length))
