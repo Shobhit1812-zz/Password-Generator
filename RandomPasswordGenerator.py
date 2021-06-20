2a9b6662a93e0237055fdfdc73f3ca02b1d5b324
Shobhit Nautiyal <84954282+Shobhit1812@users.noreply.github.com>
2021-06-20 10:39:44 +0530

    Add files via upload

---
+++ RandomPasswordGenerator.py
@@ -0,0 +1,10 @@
+import random
+
+lower="qwertyuioplkjhgfdsazxcvbnm"
+upper="QWERTYUIOPLKJHGFDSAZXCVBNM"
+numbers="0123456789"
+symbol="!@#$%^&*()_-+={[}]\|;:'""<,.>/?"
+all=lower+upper+numbers+symbol
+length=16
+password="".join(random.sample(all,length))
+print(password)
